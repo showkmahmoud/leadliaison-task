b3e15d739001e0a82ea88c6e9a18d53903a1c2e1
showk mahmoud <showkmahmoud@gmail.com>
2022-06-16 22:22:30 +0200

    components & services creation

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainPageComponent,
+    ProductsListComponent
+  ],
   imports: [
     CommonModule,
     PagesRoutingModule
